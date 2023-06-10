dbbac78d45307c631064d8b5574f486ac0456fab
wallodya <78960605+wallodya@users.noreply.github.com>
2023-06-10 12:15:03 +0300

    feat: installed tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,8 @@
 import React from "react"
-import "./App.css"
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-			</header>
+		<div>
 		</div>
 	)
 }
