5ea5e8ddf85e076bb696f43d1a919f32667cbd22
wallodya <78960605+wallodya@users.noreply.github.com>
2023-06-20 11:44:46 +0300

    feat(server):csv download

--- server/app/api/routers/order/order.py
+++ server/app/api/routers/order/order.py
@@ -1,9 +1,11 @@
 from typing import Annotated, Union
-from fastapi import APIRouter, Query
+from uuid import uuid4
+from fastapi import APIRouter, BackgroundTasks, Query
+from fastapi.responses import FileResponse
 
 from app.schemas.base import OrderData
 
-from app.crud.base import  get_orders_for_page
+from app.crud.base import  get_orders_for_page, write_orders_to_csv, remove_file
 
 from app.db.create_data import create_orders
 
@@ -13,6 +15,16 @@ order_router = APIRouter(prefix="/orders")
 @order_router.get("/")
 async def get_orders(page: Annotated[Union[int, None], Query()] = 1) -> list[OrderData]:
     return await get_orders_for_page(page)
+
+@order_router.get("/download")
+async def download_orders_table(background_tasks: BackgroundTasks):
+    
+    temp_csv_path = f"./app/api/routers/order/data/temp_orders_{str(uuid4())}.csv"
+
+    await write_orders_to_csv(temp_csv_path)
+    
+    background_tasks.add_task(remove_file, temp_csv_path)
+    return FileResponse(temp_csv_path, filename="orders.csv")
 
 @order_router.get("/create")
 async def create_fake_orders() -> None:
--- server/app/crud/base.py
+++ server/app/crud/base.py
@@ -1,6 +1,7 @@
 import time
 import uuid
 import ormar
+import os
 
 from app.models.instrument import Instrument
 from app.models.order import Order
@@ -18,8 +19,9 @@ async def get_insturment_by_id(id: int) -> Instrument:
         return server_messages.ErrorInfo(
             reason=f"Instrument with id {id} does not exists"
         )
-    
+
     return instrument
+
 
 async def create_user(user_data: UserDataIn) -> Person:
     user_id = str(uuid.uuid4())
@@ -35,20 +37,22 @@ async def create_user(user_data: UserDataIn) -> Person:
 
     return new_user
 
-async def get_orders_for_page(page: int) -> list[OrderData]:
 
-    def flatten_order_data(order: Order) -> OrderData:
-        return {
-            "order_id": order.order_id,
-            "side": order.side,
-            "amount": order.amount,
-            "price": order.price,
-            "status": order.status,
-            "user_id": order.user.uuid,
-            "instrument": order.instrument.name,
-            "created_at": order.created_at,
-            "updated_at": order.updated_at,
-        }
+def flatten_order_data(order: Order) -> OrderData:
+    return {
+        "order_id": order.order_id,
+        "side": order.side,
+        "amount": order.amount,
+        "price": order.price,
+        "status": order.status,
+        "user_id": order.user.uuid,
+        "instrument": order.instrument.name,
+        "created_at": order.created_at,
+        "updated_at": order.updated_at,
+    }
+
+
+async def get_orders_for_page(page: int) -> list[OrderData]:
 
     orders = await Order.objects.paginate(
         1 if page <= 0 else page
@@ -59,3 +63,34 @@ async def get_orders_for_page(page: int) -> list[OrderData]:
     ).all()
 
     return list(map(flatten_order_data, orders))
+
+
+async def write_orders_to_csv(path: str):
+
+    async def get_order_dicts_for_page(page: int) -> list[dict]:
+        orders = await get_orders_for_page(page)
+        return list(map(dict, orders))
+
+    def create_csv_headers(d: dict) -> str:
+        return ",".join(d.keys()) + "\n"
+
+    def create_csv_row(d: dict) -> str:
+        return ",".join(map(str, d.values())) + "\n"
+
+    with open(path, "w") as file:
+        page = 1
+        orders = await get_order_dicts_for_page(page)
+
+        file.write(create_csv_headers(orders[0]))
+
+        while len(orders) > 0:
+            for order in orders:
+                file.write(create_csv_row(order))
+            page += 1
+            orders = await get_order_dicts_for_page(page)
+
+    return
+
+def remove_file(path: str):
+    os.remove(path)
+    return
